e467c8af8bdfd0b3afbcf8e0a919a655594d958c
Mark <markmoreka@gmail.com>
2025-12-02 12:08:51 +0300

    Remove breadcrumb schema from About page and retain only FAQ and organization schemas

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -37,25 +37,7 @@ const About = () => {
     ]
   };
 
-  // Breadcrumb Schema
-  const breadcrumbSchema = {
-    "@context": "https://schema.org",
-    "@type": "BreadcrumbList",
-    "itemListElement": [
-      {
-        "@type": "ListItem",
-        "position": 1,
-        "name": "Transcontinental Investments",
-        "item": "https://transcontinentalinvestments.com/"
-      },
-      {
-        "@type": "ListItem",
-        "position": 2,
-        "name": "About Us",
-        "item": "https://transcontinentalinvestments.com/about"
-      }
-    ]
-  };
+  
 
   // Organization Schema
   const organizationSchema = {
@@ -122,10 +104,7 @@ const About = () => {
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(faqSchema) }}
       />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbSchema) }}
-      />
+      
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
